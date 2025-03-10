e96d30d0857995a0bfd9fb7d4f1814bebef90063
fbbnxfgdnh <mihlebedinskiy@gmail.com>
2025-03-10 20:42:59 +0300

    hello world!

---
+++ что ти хочещь.py
@@ -0,0 +1,11 @@
+a = int(input("Сколько яблок на складе: "))
+b = int(input("Сколько банана на складе: "))
+
+name = int(input("Ваше имя: "))
+name = int(input("Ваше имя: "))
+fruit = int(input("Что ти хочещь?: "))
+if number>a
+    print("яблок на складе хватиt")
+elif number>b
+    print("банана на складе хватит")
+else:
